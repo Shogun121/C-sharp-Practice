b10a15ce43d2ebeddb4625b597c2c287e78310ff
Shogun121 <Daimyo121@aol.com>
2016-11-22 11:29:59 -0500

    1)Created solution in VS, renamed form.

---
+++ CsharpPractice/CsharpPractice/CSsharpPracticeForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace CsharpPractice
+{
+    partial class CSsharpPracticeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CSsharpPracticeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "CSsharpPracticeForm";
+            this.Text = "Character Creation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
